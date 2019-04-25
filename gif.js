1193a97bf03a1a31120b46af11cddbca299c3818
Shelly <35303907+slane1980@users.noreply.github.com>
2019-04-25 14:34:57 -0700

    updated api key

--- gif.js
+++ gif.js
@@ -1,42 +1,38 @@
-
 /**Array of initial buttons**/
 var topics = ["puppy", "pygmy goat", "sloth", "elephant", "meerkat"];
 
-
-
 function renderButtons() {
   $("#buttons-div").empty();
-	for (var i = 0; i < topics.length; i++) {
-  	var b = $("<button>");
+  for (var i = 0; i < topics.length; i++) {
+    var b = $("<button>");
     b.addClass("btn btn-primary btn-xs animal-button");
     b.attr("id", "animal-button");
     b.attr("data-name", topics[i]);
     b.text(topics[i]);
     $("#buttons-div").append(b);
     console.log(topics[i]);
-  };
-};
+  }
+}
 
-
-function displayImages() {  
-  
+function displayImages() {
   $(".animal-button").on("click", function() {
     console.log("click!");
     var animalTopic = $(this).attr("data-name");
- 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalTopic + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      animalTopic +
+      "&api_key=PENq0OFYOmzDr7U26tJtLjqJ9Z3VdUS7&limit=10";
     // AJAX GET request
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
-      
       $("#gif-container").empty();
-     
+
       var results = response.data;
       // Loop over every result item
       for (var i = 0; i < results.length; i++) {
-       
         if (results[i].rating !== "r") {
           var gifDiv = $("<div class='item'>");
           var rating = results[i].rating;
@@ -44,7 +40,10 @@ function displayImages() {
           var animalImage = $("<img>");
           animalImage.attr("src", results[i].images.fixed_height_still.url);
           // Adding still/animate
-          animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+          animalImage.attr(
+            "data-still",
+            results[i].images.fixed_height_still.url
+          );
           animalImage.attr("data-animate", results[i].images.fixed_height.url);
           animalImage.attr("data-state", "still");
           gifDiv.append(animalImage);
@@ -53,43 +52,41 @@ function displayImages() {
           $(animalImage).on("click", function() {
             // Setting the data-state of the GIFs when clicked
             var state = $(this).attr("data-state");
-          
+
             if (state === "still") {
               $(this).attr("src", $(this).attr("data-animate"));
-            
+
               $(this).attr("data-state", "animate");
             } else {
               $(this).attr("src", $(this).attr("data-still"));
               $(this).attr("data-state", "still");
-            };
+            }
           });
-        };
-      };
+        }
+      }
     });
   });
-};
+}
 
+function remakeButtons() {
+  $("#add-topic").on("click", function(event) {
+    event.preventDefault();
 
-function remakeButtons() {	
-	$("#add-topic").on("click", function(event) {
-	  event.preventDefault();
-	 
-	  var animal = $("#inputDefault").val().trim();
-	  
-	  topics.push(animal);
-	  console.log(topics);
-	  $("#inputDefault").val("");
-	 
-	  renderButtons();
+    var animal = $("#inputDefault")
+      .val()
+      .trim();
+
+    topics.push(animal);
+    console.log(topics);
+    $("#inputDefault").val("");
+
+    renderButtons();
     displayImages();
-	});
-};
- 
-
+  });
+}
 
 $(document).ready(function() {
   remakeButtons();
- 	renderButtons();
+  renderButtons();
   displayImages();
 });
-
